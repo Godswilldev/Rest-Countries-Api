ded839033e33c612ca47b8e42e7b2330f71bdf80
Godswilldev <gimmex1@gmail.com>
2021-06-23 09:53:15 +0100

    initial commit

---
+++ src/GlobalStyles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+ *,
+  *::after,
+  *::before {
+    box-sizing: inherit;    
+    margin: 0;
+    padding: 0;
+  }
+  html{
+      font-size: 62.5%;
+      box-sizing: border-box;
+  }
+
+  body {
+font-family: 'Nunito Sans', sans-serif;   
+ transition:.3s all;
+         background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+
+
+  }
+`;
