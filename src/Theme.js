93967336cc4df05ccdd89c2173eba57e6a2d85a5
Godswilldev <gimmex1@gmail.com>
2021-06-23 09:53:44 +0100

    initial commit

---
+++ src/Theme.js
@@ -0,0 +1,10 @@
+export const light = {
+  body: "#fafafa",
+  text: "#111517",
+  elements: "#ffffff",
+};
+export const dark = {
+  body: "#202c37",
+  text: "#fafafa",
+  elements: "#2b3945",
+};
