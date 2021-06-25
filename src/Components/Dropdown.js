2b57d5b1581e34f3c4426d60cf78c9d6f54c8c2c
Godswill Edet <gimmex1@gmail.com>
2021-06-25 21:24:06 +0100

    implemented dropdown functionality

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { CountriesContext } from "../Context/CountriesContext";
 
-const Dropdown = () => {
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+    color: theme.text,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function Dropdown() {
+  const classes = useStyles();
+  const { dispatch, search } = useContext(CountriesContext);
+
   return (
     <div>
-      <h1>Dropdown goes here</h1>
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel id="demo-simple-select-outlined-label">
+          Filter by Region
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={search}
+          onChange={(evt) =>
+            dispatch({
+              type: "HANDLE INPUT",
+              name: evt.target.name,
+              payload: evt.target.value,
+            })
+          }
+          label="Filter by Region"
+          name="search"
+        >
+          <MenuItem value="">All</MenuItem>
+          <MenuItem value="africa">Africa</MenuItem>
+          <MenuItem value="america">America</MenuItem>
+          <MenuItem value="asia">Asia</MenuItem>
+          <MenuItem value="europe">Europe</MenuItem>
+          <MenuItem value="australia">Australia</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
-};
-
-export default Dropdown;
+}
